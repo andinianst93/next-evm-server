f11b4bc5cda2353f3a904bf47a0fccdfa96238ea
andinianst93 <andinianst93@gmail.com>
2023-02-16 06:26:15 +0700

    first commit

---
+++ src/api/edu/controllers/edu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * edu controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::edu.edu');
---
+++ src/api/edu/services/edu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * edu service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::edu.edu');
